9ce7f6819cfdf8a5414c04c920a2316cc5421c0b
Martin Mende <martin.mende@aristech.de>
2024-10-24 11:57:09 +0200

    🛠️ Small changes in build.rs

--- rust/build.rs
+++ rust/build.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let proto_file = "./protos/stt_service.proto";
+    let proto_file = "protos/stt_service.proto";
 
     tonic_build::configure()
         .build_server(false)
-        .compile_protos(&[proto_file], &["."])
+        .compile_protos(&[proto_file], &["protos"])
         .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
 
     println!("cargo:rerun-if-changed={}", proto_file);
