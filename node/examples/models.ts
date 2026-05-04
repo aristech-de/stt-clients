ac66a5b5ce9e694a527f8add53d9269f393194a9
Martin Mende <martin.mende@aristech.de>
2026-05-04 10:25:36 +0200

    NodeJS v2.5.0

--- node/examples/models.ts
+++ node/examples/models.ts
@@ -1,17 +1,18 @@
 import 'dotenv/config'
 
-import { SttClient, endpointingTypeToJSON, grammarTypeToJSON, modelTypeToJSON } from '@aristech-org/stt-client'
+import { SttClient, endpointingTypeToJSON, grammarTypeToJSON, languageSelectionModeToJSON, modelTypeToJSON } from '@aristech-org/stt-client'
 
 const client = new SttClient()
 const models = await client.listModels()
 
 for (const model of models) {
   // We pull out the enum values and convert them to their string representation for better readability
-  const { grammarType, type, endpointing, ...rest } = model
+  const { grammarType, type, endpointing, languageSelectionMode, ...rest } = model
   console.log({
     ...rest,
     type: modelTypeToJSON(type),
     grammarType: grammarType.map((grammarType) => grammarTypeToJSON(grammarType)),
     endpointing: endpointing.map((endpointingType) => endpointingTypeToJSON(endpointingType)),
+    languageSelectionMode: languageSelectionModeToJSON(languageSelectionMode),
   })
 }
